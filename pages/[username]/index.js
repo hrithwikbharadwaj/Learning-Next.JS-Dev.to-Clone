b941e37d4d9bf2569154d1d5c1665afc2f45bfb3
hrithwikbharadwaj <hrithwik009@gmail.com>
2021-08-04 17:30:15 +0530

    USer Profile done

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,40 @@
-export default function username({ }) {
+import { getUserWithUsername, postToJson } from "../../lib/firebase"
+import UserProfile from '../../components/UserProfile'
+import PostFeed from '../../components/PostFeed';
+
+export async function getServerSideProps({ query }) {
+    const { username } = query
+    const userDoc = await getUserWithUsername(username)
+    // if (!userDoc) {
+    //     return {
+    //         notFound: true,
+    //     };
+    // }
+
+    let user = null;
+    let posts = null;
+
+    if (userDoc) {
+
+        user = userDoc.data();
+        const postsQuery = userDoc.ref
+            .collection('posts')
+            .where('published', '==', true)
+            .orderBy('createdAt', 'desc')
+            .limit(5)
+        posts = (await postsQuery.get()).docs.map(postToJson)
+    }
+
+    return {
+        props: { user, posts }
+    };
+}
+
+export default function userPrfilePage({ user, posts }) {
     return (
         <main>
-            Hey User
+            <UserProfile user={user} />
+            <PostFeed posts={posts} />
         </main>
     )
 }
